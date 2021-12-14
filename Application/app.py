d59eecf3851032b80eec621709ff498dc0294ca1
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-12-14 12:44:02 +0000

    changes

--- Application/app.py
+++ Application/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_internet():
-    return "Hello Changed Pipeline!"
+    return "Hello Changed Pipeline! This is automatically built and pushed to DockerHub."
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
