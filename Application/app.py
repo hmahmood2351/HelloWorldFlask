2c0c044d60d54a86ff6739cc413b8e69164abab8
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-12-14 21:44:58 +0000

    changes

--- Application/app.py
+++ Application/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_internet():
-    return "Hello New Image!"
+    return "Hello Automated Deployment!"
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
