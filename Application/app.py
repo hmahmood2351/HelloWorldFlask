4981d7558003ce860244e7aa993dc7e3b01329dd
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-12-15 10:30:35 +0000

    changes

--- Application/app.py
+++ Application/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 import platform
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"Hello Automated Deployment! This is built originally on {platform.node()}"
+    return render_template('index.html') 
+    #f"Hello Automated Deployment! This is built originally on {platform.node()}"
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
