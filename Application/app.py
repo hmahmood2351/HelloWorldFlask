f4f65bae64cbdf262e2e6aeefc3f65af0b2d17ec
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-12-15 09:39:24 +0000

    changes

--- Application/app.py
+++ Application/app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-import socket
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_internet():
-    return f"Hello Automated Deployment! This is running on {socket.gethostname()}"
+    return f"Hello Automated Deployment! This is built originally on {os.uname()}"
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
