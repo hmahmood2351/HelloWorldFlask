87053c55511e7ca69171ccd5f0595b462e8078f1
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-12-15 16:51:51 +0000

    change

--- Application/app.py
+++ Application/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"Hello PAIN! {os.getenv(connectionstring)}"
+    return f"Hello PAIN! {os.getenv('connectionstring')}"
     
     #render_template('index.html') 
     #f"Hello Automated Deployment! This is built originally on {platform.node()}"
