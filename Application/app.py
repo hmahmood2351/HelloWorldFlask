108b955a60dec555595c70495b541c712b2ea494
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-12-15 19:23:51 +0000

    change

--- Application/app.py
+++ Application/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"Hello PAINNEW! {os.getenv('DATABASE_URI')}"
+    return f"Hello PAINNEW2! {os.getenv('DATABASE_URI')}"
     
     #render_template('index.html') 
     #f"Hello Automated Deployment! This is built originally on {platform.node()}"
