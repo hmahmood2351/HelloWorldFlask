36cceba642f612d13008df94038d3126d9aace01
MohammedHassanMahmood <hmahmood2351@protonmail.com>
2021-12-15 09:42:10 +0000

    changes

--- Application/app.py
+++ Application/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_internet():
-    return f"Hello Automated Deployment! This is built originally on {os.uname()}"
+    return f"Hello Automated Deployment! This is built originally on {os.cmd(hostname)}"
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
